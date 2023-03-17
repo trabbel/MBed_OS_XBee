8bd90991d652c28b8ed9023b1c2301c7d03f7e9e
Tim-Lucas Rabbel <timlucas.rabbel@student.uni-luebeck.de>
2023-03-17 14:20:53 +0000

    Fixed bug: escaping length and checksum

--- zigbee.cpp
+++ zigbee.cpp
@@ -6,48 +6,57 @@
 #define START_DELIMITER 0x7E
 #define ESCAPE_CHAR     0x7D
 
+/**
+ * Write a xBee API 2 in a buffer
+ * 
+ * This function assembles a 0x10 transmit request frame and writes it in a given buffer.
+ * The frame can be send easily by writing the buffer to a serial port that is connected to 
+ * a xBee device. 
+
+ */
 int writeFrame(char *frame, int addr16, uint64_t addr64, char *payload, int payloadSize){
     // Define a temporary array of sufficient size for everything that may be escaped
-    char tempFrame[(13+payloadSize)];
+    char tempFrame[(16+payloadSize)];
     // Frame delimiter
     frame[0] = START_DELIMITER;
     // Payload length starting with frame type byte, excluding checksum and neccessary escape characters
     int frame_len = 0x0E + payloadSize;
-    frame[1] = (frame_len >> 8) & 0xFF;
-    frame[2] = frame_len & 0xFF;
-    // Frame type (TX request)
-    frame[3] = 0x10;
+    tempFrame[0] = (frame_len >> 8) & 0xFF;
+    tempFrame[1] = frame_len & 0xFF;
+    // API ID (TX request)
+    tempFrame[2] = 0x10;
     // Frame ID
-    tempFrame[0] = 0x01;
+    tempFrame[3] = 0x01;
     // 64-bit destination address
     for(int i = 0; i<8; i++){
-        tempFrame[1+i] = (addr64 >> (7-i)*8) & 0xFF;
+        tempFrame[4+i] = (addr64 >> (7-i)*8) & 0xFF;
     }
     // 16-bit address
-    tempFrame[9] = (addr16 >> 8) & 0xFF;
-    tempFrame[10] = (addr16 >> 0) & 0xFF;
+    tempFrame[12] = (addr16 >> 8) & 0xFF;
+    tempFrame[13] = (addr16 >> 0) & 0xFF;
     // Broadcast radius
-    tempFrame[11] = 0x00;
+    tempFrame[14] = 0x00;
     // Options
-    tempFrame[12] = 0x00;
+    tempFrame[15] = 0x00;
     // Payload
-    memcpy(&tempFrame[13], payload, payloadSize);
+    memcpy(&tempFrame[16], payload, payloadSize);
     // Checksum is calculated based on the original data (non-escaped payload),
     // starting with the frame type
-    char checksum = frame[3];
-    for(int i = 0; i< 13 + payloadSize; i++){
+    char checksum = 0x00;
+    for(int i = 2; i< 16 + payloadSize; i++){
         checksum += tempFrame[i];
     }
+    tempFrame[16+payloadSize] = 0xFF-checksum;
     // Escape the payload and count how many additional escape characters have to be transitted
     int escapes = escapePayload(tempFrame, frame, payloadSize);
     // Write the checkSum at the end of the frame
-    frame[17+payloadSize + escapes] = 0xFF - checksum;
+    //frame[17+payloadSize + escapes] = 0xFF - checksum;
 
     for (int i = 0 ; i<18+payloadSize+escapes; i++){
             debug("%02x ", tempFrame[i]);
     }debug("\n");
 
-    // The frame is now written in the buffer and ready to send. Return the frame size in bytes
+    // The frame is now written in the buffer and ready to send
     return 18+payloadSize+escapes;
 }
 
@@ -55,13 +64,20 @@ int escapePayload(char* payload, char* tx_buf, int payloadSize){
     // Counter for needed escape characters
     int escapes = 0;
     // Where to start writing the payload in the buffer
-    int pos = 4;
-    for (int i = 0; i < 13 + payloadSize; i++){
+    int pos = 1;
+    // Go through each payload byte
+    for (int i = 0; i < 17 + payloadSize; i++){
+        // Check if the current byte has to be escaped 
+        // 0x7e         | frame delimiter, signifies a new frame
+        // 0x7d         | escape character, signifies that the next character is escaped
+        // 0x11, 0x13   | software control character
         if (payload[i] == 0x7E || payload[i] == 0x7D || payload[i] == 0x11 || payload[i] == 0x13){
+            // If a byte has to be escaped, first write ox7d, followed by the byte XORed with 0x20
             tx_buf[pos++] = 0x7D;
             tx_buf[pos++] = payload[i] ^ 0x20;
             escapes++;
         }else{
+            // Otherwise write the byte
             tx_buf[pos++] = payload[i];
         }
     }
@@ -69,19 +85,36 @@ int escapePayload(char* payload, char* tx_buf, int payloadSize){
 }
 
 parsedFrame readFrame(char *frame, BufferedSerial &serial){
-    char API_ID = 0;
+    //char API_ID = 0;
     parsedFrame result = {0x00, 0};
     debug("Frame start\n");
 
     unsigned char lengthBytes[2];
-    serial.read(lengthBytes, 2);
+    
+    for (int i = 0; i < 2; i++) {
+        if (!serial.readable()>0) {
+            debug("Serial not readable with i: %i\n", i);
+            return result;
+        }
+        serial.read(&lengthBytes[i], 1);
+        if (lengthBytes[i] == START_DELIMITER) {
+            debug("START_DELIMITER, should have been data\n");
+            return result;
+        }else if(lengthBytes[i] == ESCAPE_CHAR){
+            thread_sleep_for(1);
+            serial.read(&lengthBytes[i], 1);
+            lengthBytes[i] = lengthBytes[i] ^ 0x20;
+        }
+        thread_sleep_for(1);
+    }
+
     int frameLength = (lengthBytes[0] << 8) + lengthBytes[1];
 
-    serial.read(&API_ID, 1);
-    int payloadSize = frameLength -1;
+    //serial.read(&API_ID, 1);
+    int payloadSize = frameLength;
 
 
-    for (int i = 0; i < payloadSize; i++) {
+    for (int i = 0; i < payloadSize + 1; i++) {
         if (!serial.readable()>0) {
             debug("Serial not readable with i: %i\n", i);
             return result;
@@ -97,19 +130,19 @@ parsedFrame readFrame(char *frame, BufferedSerial &serial){
         }
         thread_sleep_for(1);
     }
-    serial.read(&frame[payloadSize], 1);
-    char checksum = API_ID;
+    //serial.read(&frame[payloadSize], 1);
+    char checksum = 0x00;
     for(int i = 0; i< payloadSize; i++){
         checksum += frame[i];
     }
     if (frame[payloadSize] == 0xFF - checksum){
         debug("Checksum correct!\n");
     }else{
-        debug("Checksum wrong! Should have been %02x\n", frame[payloadSize]);
-        return result;
+        debug("Checksum wrong! Should have been %02x, was %02x\n", frame[payloadSize], 0xFF -checksum);
+        //return result;
     }
 
     debug("Complete Frame parsed\n");
-    result = {API_ID, payloadSize};
+    result = {frame[0], payloadSize};
     return result;
 }
